c009326c7d47acd8ae1af4a07096b2a261964cce
Ryan <>
2020-08-14 18:54:07 +0900

    jhlee - 추가파일.

---
+++ AppStoreSearch/Request/Response/ResponseReceiver.swift
@@ -0,0 +1,23 @@
+//
+//  ResponseReceiver.swift
+//  AppStoreSearch
+//
+//  Created by 이정환 on 2020/08/14.
+//  Copyright © 2020 RYAN. All rights reserved.
+//
+
+import Foundation
+
+class ResponseReceiver<T: BaseObject> {
+    
+    typealias CompletionHandler = (_ status: ResponseStatus, _ object: T?, _ error: ApiError?) -> Void
+    private var receiver: CompletionHandler?
+    
+    init(_ receiver: @escaping CompletionHandler) {
+        self.receiver = receiver
+    }
+    
+    func get() -> CompletionHandler? {
+        return self.receiver
+    }
+}
